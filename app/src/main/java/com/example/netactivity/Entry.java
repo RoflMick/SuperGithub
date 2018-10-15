8c0fc85e71facb01278a525bb355bd604d303ad4
RoflMick <roflmick@seznam.cz>
2018-10-15 17:16:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/netactivity/Entry.java
@@ -0,0 +1,16 @@
+package com.example.netactivity;
+
+public class Entry {
+    public final String zeme;
+    public final String kod;
+    public final String kurz;
+ 
+    // TODO 3. Rozsirit dalsi udaje ve tride, ktere se budou vest pro kazdou menu
+    // TODO 3. To zahrnuje i upraveni konstruktoru
+            
+    Entry(String zeme, String kod, String kurz) {
+        this.zeme = zeme;
+        this.kod = kod;
+        this.kurz = kurz;
+    }
+}
